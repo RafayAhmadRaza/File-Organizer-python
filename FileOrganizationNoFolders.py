e98eb9950b00e7ff1c95b7019dd65ff52fe32d04
RafayAhmadRaza <rafayahmadraza@gmail.com>
2024-07-17 13:26:28 +0500

    Add the script which copies without any folders creating

---
+++ FileOrganizationNoFolders.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import os
+import shutil
+
+SourcePath = str(input("Enter Path of Source: ")) 
+p = Path(SourcePath) #Write Path to Location from where you need to get files to move
+
+while True:
+    if p.exists():
+        print("Source Path Opened")
+        break
+    else:
+        SourcePath = str(input("ReEnter Path Of Source: "))
+        p = Path(SourcePath)
+
+DescPath = str(input("Enter The Destination Path: "))
+
+move_dir = Path(DescPath) #Directory where it is being moved
+
+
+if not move_dir.exists():
+    os.mkdir(move_dir)
+else:
+    print("Folder has been created already")
+
+fileExtension = str(input("Enter file extension(eg: *.pdf here remember to add * before the extension, you could also specifiy FileName*.extension): "))
+
+for filename in p.glob(fileExtension): # in glob you need to add the file extension
+
+    #shows file name
+    print(filename)
+    #Creates a new name without suffix eg you have a pdf file you remove the .pdf from it
+
+    #creates the location path where the file needs to be stored
+    shutil.move(filename,move_dir)
